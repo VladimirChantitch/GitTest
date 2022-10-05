a2f8934f598cc1ac4ccfbbaee4375ae9cc400658
Vladimir Chantitch <chantitch.vladimir@gmail.com>
2022-10-05 12:03:21 +0200

    DOCUMENTATION

--- GitTest/GitTest/Program.cs
+++ GitTest/GitTest/Program.cs
@@ -6,19 +6,31 @@ using System.IO;
 
 public class TestGit
 {
+    /// <summary>
+    /// Singleton
+    /// </summary>
     public static TestGit? Instance { get; private set; }
 
+    /// <summary>
+    /// Constructor to set up singleton
+    /// </summary>
     TestGit()
     {
         Instance = this;
     }
 
+    /// <summary>
+    /// Start of the solution 
+    /// </summary>
     static void Main()
     {
         new TestGit();
         Instance?.RunSolution();
     }
 
+    /// <summary>
+    /// Run the solution, contains the loop of logic
+    /// </summary>
     void RunSolution()
     {
         while (true)
@@ -27,6 +39,9 @@ public class TestGit
         }
     }
 
+    /// <summary>
+    /// A fonction to selec te type of calculation
+    /// </summary>
     void SelectTypeOfCalculation()
     {
         Console.WriteLine("Which type of calculation do you want \r");
@@ -40,6 +55,9 @@ public class TestGit
         }
     }
 
+    /// <summary>
+    /// A fonction to calculate the powr of a number depending on a further read line statement
+    /// </summary>
     void CalculatePower()
     {
         Console.WriteLine("Pleas enter an int for which you need the power");
